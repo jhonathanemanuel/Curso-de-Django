6aec6a7f5a0da7d23f0b8768f9ea6364208b1fc5
Jhonathan Emanuel <jhonathanemanuelgg@gmail.com>
2026-03-17 15:15:01 -0300

    Criando as rotas e o index.html

--- Aula_1/learning_log/learning_logs/views.py
+++ Aula_1/learning_log/learning_logs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """""Página principal do learning_log"""
+    return render(request, 'learning_logs/index.html')
